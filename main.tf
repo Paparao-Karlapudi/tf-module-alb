32017af06621b055c7d288fd4c0b62ea769e272e
pappik <pappik@gmail.com>
2023-11-15 18:32:12 -0500

    documentdb

--- main.tf
+++ main.tf
@@ -23,13 +23,13 @@ resource "aws_security_group" "main" {
 }
 
 resource "aws_lb" "main" {
-  name               = "${var-env}-${var.subnets_name}-alb"
+  name               = "${var.env}-${var.subnets_name}-alb"
   internal           = var.internal
   load_balancer_type = "application"
   security_groups    = [aws_security_group.main.id]
   subnets            = var.subnet_ids
 
-  tags = merge(local.common_tags, { Name = "${var-env}-${var.subnets_name}-alb" } )
+  tags = merge(local.common_tags, { Name = "${var.env}-${var.subnets_name}-alb" } )
 
 }
 
